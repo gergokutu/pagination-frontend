2dbd55b306d391f70b29b0ff96cd60854d4ea937
gergokutu <gergo.kutu@gmail.com>
2019-10-03 13:04:25 +0200

    Bootstrap applied, data fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+import Posts from './components/Posts';
 
 function App() {
+  const [ posts, setPosts ] = useState([])
+  const [ loading, setLoading ] = useState(false)
+  const [ currentPage, setCurrentPage ] = useState(1)
+  const [ postsPerPage, setPostsPerPage ] = useState(10)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+      const res = await Axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(res.data)
+      setLoading(false)
+    }
+
+    fetchPosts()
+  // [] » in order to only run it 1x when it mounts
+  // not to have a infinite loop
+  }, [])
+
   return (
-    <div className='container'>
-      <h1>My App</h1>
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={posts} loading={loading} />
     </div>
   );
 }
